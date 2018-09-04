102cd00569eca4ddb76dfee7a0effa3e3b2898f6
rzonsol <rzonsol@gmail.com>
2018-09-04 23:38:57 +0200

    add port number to dto

--- src/main/java/pl/rzonsol/microservices/currencyexchangeservice/contraoller/CurrencyExchangeController.java
+++ src/main/java/pl/rzonsol/microservices/currencyexchangeservice/contraoller/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package pl.rzonsol.microservices.currencyexchangeservice.contraoller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,9 @@ import java.math.BigDecimal;
 @RestController
 public class CurrencyExchangeController {
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to){
 
@@ -18,6 +23,7 @@ public class CurrencyExchangeController {
                 .from("EUR")
                 .to("PLN")
                 .conversionMultiple(BigDecimal.valueOf(4.3086))
+                .port(Integer.parseInt(environment.getProperty("local.server.port")))
                 .build();
     }
 }
--- src/main/java/pl/rzonsol/microservices/currencyexchangeservice/model/ExchangeValue.java
+++ src/main/java/pl/rzonsol/microservices/currencyexchangeservice/model/ExchangeValue.java
@@ -15,4 +15,5 @@ public class ExchangeValue {
     private String from;
     private String to;
     private BigDecimal conversionMultiple;
+    private int port;
 }
