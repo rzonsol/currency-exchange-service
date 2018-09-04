a8f81170a5efce81b953b39d5e3127816f5109ac
rzonsol <rzonsol@gmail.com>
2018-09-05 00:00:02 +0200

    enable h2 db

--- src/main/java/pl/rzonsol/microservices/currencyexchangeservice/model/ExchangeValue.java
+++ src/main/java/pl/rzonsol/microservices/currencyexchangeservice/model/ExchangeValue.java
@@ -2,17 +2,24 @@ package pl.rzonsol.microservices.currencyexchangeservice.model;
 
 import lombok.*;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
 @Getter
 @Setter
+@Entity
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class ExchangeValue {
 
+    @Id
     private Long id;
+    @Column(name = "currency_from")
     private String from;
+    @Column(name = "currency_to")
     private String to;
     private BigDecimal conversionMultiple;
     private int port;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values (1, 'EUR', 'PLN', 4.3086, 0);
+INSERT into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values (2, 'USD', 'PLN', 3.7241, 0);
+INSERT into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values (3, 'CHF', 'PLN', 3.8287, 0);
+INSERT into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values (4, 'CZK', 'PLN', 0.1673, 0);
