598df7e50c04753f934c67605a685206a479fa49
rzonsol <rzonsol@gmail.com>
2018-09-07 11:02:24 +0200

    add sleuth and logging

--- src/main/java/pl/rzonsol/microservices/currencyexchangeservice/contraoller/CurrencyExchangeController.java
+++ src/main/java/pl/rzonsol/microservices/currencyexchangeservice/contraoller/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package pl.rzonsol.microservices.currencyexchangeservice.contraoller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,8 @@ import pl.rzonsol.microservices.currencyexchangeservice.repository.ExchangeValue
 @RestController
 public class CurrencyExchangeController {
 
+    private final static Logger LOG = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
     @Autowired
     private Environment environment;
 
@@ -20,7 +24,7 @@ public class CurrencyExchangeController {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to){
-
+        LOG.info("Conversion from: "+ from + " to: "+ to);
         ExchangeValue exchangeValue = exchangeValueRepository.findByFromAndTo(from,to).orElseThrow(()-> new ConversionNotFoundException("Conversion from: "+ from + " to: "+ to + " not found in DB" ));
 
         exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
